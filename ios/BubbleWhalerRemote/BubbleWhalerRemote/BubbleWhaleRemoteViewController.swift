ed5eaf10292488d2f20453f63a86eb8b6412ae18
Penjat <theSpencerSymington@Gmail.com>
2021-03-25 22:03:06 -0700

    Turning on and off with serial.

--- ios/BubbleWhalerRemote/BubbleWhalerRemote/BubbleWhaleRemoteViewController.swift
+++ ios/BubbleWhalerRemote/BubbleWhalerRemote/BubbleWhaleRemoteViewController.swift
@@ -2,23 +2,51 @@ import UIKit
 import CoreBluetooth
 
 class BubbleWhaleRemoteViewController: UIViewController {
-	var centralManager: CBCentralManager!
+	//	var centralManager: CBCentralManager!
+	var bluetoothSerial: BluetoothSerial!
 	var bubbleWhalePeripheral: CBPeripheral!
+	var myChar: CBCharacteristic?
 	lazy var mainStack: UIStackView = {
 		let stack = UIStackView()
 		stack.axis = .vertical
+		stack.distribution = .fillEqually
 		return stack
 	}()
 	let bubbleWhaleStatusLabel: UILabel = {
 		let label = UILabel()
+		label.textAlignment = .center
 		label.text = ""
 		return label
+	}()
+	let bubbleWhaleActiveLabel: UILabel = {
+		let label = UILabel()
+		label.textAlignment = .center
+		label.text = ""
+		return label
+	}()
+
+	let onButton: UIButton = {
+		let button = UIButton()
+		button.setTitle("ON", for: .normal)
+		button.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+		button.addTarget(self, action: #selector(pressedOn), for: .touchUpInside)
+		return button
+	}()
+
+	let offButton: UIButton = {
+		let button = UIButton()
+		button.setTitle("OFF", for: .normal)
+		button.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+		button.addTarget(self, action: #selector(pressedOff), for: .touchUpInside)
+		return button
 	}()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setUpViews();
-		centralManager = CBCentralManager(delegate: self, queue: nil)
+		//		centralManager = CBCentralManager(delegate: self, queue: nil)
+		bluetoothSerial = BluetoothSerial(delegate: self)
+
 	}
 
 	public func setUpViews() {
@@ -31,42 +59,83 @@ class BubbleWhaleRemoteViewController: UIViewController {
 		mainStack.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
 
 		mainStack.addArrangedSubview(bubbleWhaleStatusLabel)
+		mainStack.addArrangedSubview(bubbleWhaleActiveLabel)
+		mainStack.addArrangedSubview(onButton)
+		mainStack.addArrangedSubview(offButton)
+	}
+
+	@objc
+	func pressedOn() {
+		let data = "o".data(using: .utf8)!
+		bluetoothSerial.sendDataToDevice(data)
+	}
+
+	@objc
+	func pressedOff() {
+		let data = "f".data(using: .utf8)!
+		bluetoothSerial.sendDataToDevice(data)
 	}
 }
 
-extension BubbleWhaleRemoteViewController
-: CBCentralManagerDelegate {
-	func centralManagerDidUpdateState(_ central: CBCentralManager) {
-		switch central.state {
-		  case .unknown:
+extension BubbleWhaleRemoteViewController: BluetoothSerialDelegate {
+	func serialDidChangeState() {
+
+		switch bluetoothSerial.centralManager.state {
+		case .unknown:
 			print("central.state is .unknown")
-		  case .resetting:
+		case .resetting:
 			print("central.state is .resetting")
-		  case .unsupported:
+		case .unsupported:
 			print("central.state is .unsupported")
-		  case .unauthorized:
+		case .unauthorized:
 			print("central.state is .unauthorized")
-		  case .poweredOff:
+		case .poweredOff:
 			print("central.state is .poweredOff")
-		  case .poweredOn:
+		case .poweredOn:
 			print("central.state is .poweredOn")
-			bubbleWhaleStatusLabel.text = "Scanning for buuble whale..."
-			self.centralManager?.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey:false])
-		}
-	}
-	func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-		if advertisementData["kCBAdvDataLocalName"] != nil {
-			print("Bubble-Whale-Detected");
-			bubbleWhaleStatusLabel.text = "Bubble-Whale-Detected..."
-			bubbleWhalePeripheral = peripheral
-			centralManager.stopScan()
-			centralManager.connect(bubbleWhalePeripheral)
+			bubbleWhaleStatusLabel.text = "Scanning..."
+			bluetoothSerial.startScan()
 		}
 	}
 
-	func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-	  print("Bubble Whale Connected!")
-		bubbleWhaleStatusLabel.text = "Connected to Bubble Whale"
-//		bubbleWhalePeripheral.discoverServices(nil)
+
+
+	func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
+
+	}
+
+	func serialDidDiscoverPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?) {
+		print(peripheral)
+		if peripheral.name == "Bubble-Whale" {
+			bubbleWhaleStatusLabel.text = "Bubble Whale detected, attempting to connect..."
+			bluetoothSerial.connectToPeripheral(peripheral)
+		}
+	}
+	func serialDidFailToConnect(_ peripheral: CBPeripheral, error: NSError?) {
+		bubbleWhaleStatusLabel.text = "Failed to connect."
+	}
+	func serialIsReady(_ peripheral: CBPeripheral){
+		bubbleWhaleStatusLabel.text = "Bubble Whale Connected."
+		let data = "f".data(using: .utf8)!
+		bluetoothSerial.sendDataToDevice(data)
+		print("serial is ready")
+	}
+	func serialDidReceiveData(_ data: Data){
+		print("recieved data: \(data)")
+	}
+
+	func serialDidReceiveBytes(_ bytes: [UInt8]) {
+		print("did recieve bytes: \(bytes)")
+	}
+
+	func serialDidReceiveString(_ message: String) {
+		print("did recieve string: \(message)")
+		if message == "WHALE-ON" {
+			bubbleWhaleActiveLabel.text = "The Whale is making bubles."
+		}
+
+		if message == "WHALE-OFF" {
+			bubbleWhaleActiveLabel.text = "The Whale is not making bubles."
+		}
 	}
 }
