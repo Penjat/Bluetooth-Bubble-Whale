f6e557a332fb6b7fd307f831b09ed07e6a0c4448
Penjat <theSpencerSymington@Gmail.com>
2021-03-25 17:17:04 -0700

    ISO: Basic Set up of app, connects to whale.

---
+++ ios/BubbleWhalerRemote/BubbleWhalerRemote/BubbleWhaleRemoteViewController.swift
@@ -0,0 +1,60 @@
+import UIKit
+import CoreBluetooth
+
+class BubbleWhaleRemoteViewController: UIViewController {
+	var centralManager: CBCentralManager!
+	var bubbleWhalePeripheral: CBPeripheral!
+	lazy var mainStack: UIStackView = {
+		let stack = UIStackView()
+		stack.axis = .vertical
+		return stack
+	}()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setUpViews();
+		centralManager = CBCentralManager(delegate: self, queue: nil)
+	}
+
+	public func setUpViews() {
+		view.addSubview(mainStack)
+		mainStack.translatesAutoresizingMaskIntoConstraints = false
+		mainStack.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+		mainStack.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+		mainStack.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor).isActive = true
+		mainStack.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor).isActive = true
+	}
+}
+
+extension BubbleWhaleRemoteViewController
+: CBCentralManagerDelegate {
+	func centralManagerDidUpdateState(_ central: CBCentralManager) {
+		switch central.state {
+		  case .unknown:
+			print("central.state is .unknown")
+		  case .resetting:
+			print("central.state is .resetting")
+		  case .unsupported:
+			print("central.state is .unsupported")
+		  case .unauthorized:
+			print("central.state is .unauthorized")
+		  case .poweredOff:
+			print("central.state is .poweredOff")
+		  case .poweredOn:
+			print("central.state is .poweredOn")
+			self.centralManager?.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey:false])
+		}
+	}
+	func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+		if advertisementData["kCBAdvDataLocalName"] != nil {
+			print("Bubble-Whale-Detected");
+			bubbleWhalePeripheral = peripheral
+			centralManager.stopScan()
+			centralManager.connect(bubbleWhalePeripheral)
+		}
+	}
+
+	func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+	  print("Bubble Whale Connected!")
+	}
+}
