baadcecae5b6310cd3c97c09931745d53be62e5d
Penjat <theSpencerSymington@Gmail.com>
2021-04-07 12:48:19 -0700

    UI looking good.

--- ios/BubbleWhalerRemote/BubbleWhalerRemote/Remote/View/RemoteSwiftUIView.swift
+++ ios/BubbleWhalerRemote/BubbleWhalerRemote/Remote/View/RemoteSwiftUIView.swift
@@ -2,8 +2,19 @@ import SwiftUI
 
 struct RemoteSwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+		VStack(alignment: .center, spacing: 20) {
+			Text("Bubble Whale Status")
+			Spacer()
+			Image("Whale")
+			Text("Bubble Status")
+			Spacer()
+			Button("ON") {
+				print("Button tapped!")
+			}.padding(32)
+			.border(Color.black, width: 2)
+
+		}.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+	}
 }
 
 struct RemoteSwiftUIView_Previews: PreviewProvider {
